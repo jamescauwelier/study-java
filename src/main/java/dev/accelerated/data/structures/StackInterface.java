da223af05b292530f2c1f5d401d214d031ef291b
James Cauwelier <james@accelerated.dev>
2025-01-03 09:00:49 -0800

    Removes redundant visibility modifiers

--- src/main/java/dev/accelerated/data/structures/StackInterface.java
+++ src/main/java/dev/accelerated/data/structures/StackInterface.java
@@ -6,22 +6,22 @@ public interface StackInterface<T> {
     /**
      * Pushes a value onto the stack.
      */
-    public void push(T value);
+    void push(T value);
 
     /**
      * Pops the value that was last added off the stack. This value may
      * or may not be present
      */
-    public Optional<T> pop();
+    Optional<T> pop();
 
     /**
      * Similar to pop, but does not remove the value from the stack, it
      * just returns it, so it can be inspected.
      */
-    public Optional<T> peek();
+    Optional<T> peek();
 
     /**
      * Returns the number of elements currently in the stack.
      */
-    public int size();
+    int size();
 }
